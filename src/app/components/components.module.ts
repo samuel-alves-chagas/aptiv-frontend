354ba68e09bfdc861fbd2e65d1351a408a74a3c2
Samuel Alves Chagas <samuelchagasalves@gmail.com>
2023-10-05 21:25:30 -0300

    header

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,19 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SelectCardsComponent } from './select-cards/select-cards.component';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './header/header.component';
 
 
 
 @NgModule({
   declarations: [
-    SelectCardsComponent
+    SelectCardsComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule,
     NgbDropdownModule
   ],
   exports:[
-    SelectCardsComponent
+    SelectCardsComponent,
+    HeaderComponent
   ]
 })
 export class ComponentsModule { }
