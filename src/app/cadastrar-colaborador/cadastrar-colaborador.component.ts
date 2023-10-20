42115c768fe0fc9448d0dc9c77cc7f8b1ab32b48
Daiane Dionisio de Lima <daianedionisio2000@gmail.com>
2023-10-19 23:00:10 -0300

    Tela de cadastro de novo colaborador

--- src/app/cadastrar-colaborador/cadastrar-colaborador.component.ts
+++ src/app/cadastrar-colaborador/cadastrar-colaborador.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadastrar-colaborador',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./cadastrar-colaborador.component.css']
 })
 export class CadastrarColaboradorComponent {
+  public formCadastrarColaborador: FormGroup
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    
+  ) {
+    this.formCadastrarColaborador = this.formBuilder.group({
+      nome: [null],
+      dataDeAdmissao: [null],
+      unidade: [null],
+      cargo: [null],
+      departamento: [null],
+      perfilDeAcesso: [null]
+    })
+  }
+
+  cadastrar() {
+    console.log(this.formCadastrarColaborador)
+  }
 }
