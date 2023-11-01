5b5122e10417afc8f371727bdaa88a859cb10ce5
Daiane Dionisio de Lima <daianedionisio2000@gmail.com>
2023-11-01 01:16:34 -0300

    Tela de cadastro do colaborador

--- src/app/cadastrar-colaborador/cadastrar-colaborador.component.ts
+++ src/app/cadastrar-colaborador/cadastrar-colaborador.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -13,17 +13,30 @@ export class CadastrarColaboradorComponent {
   constructor(
     private formBuilder: FormBuilder,
     private router: Router,
-    
   ) {
     this.formCadastrarColaborador = this.formBuilder.group({
-      nome: [null],
-      dataDeAdmissao: [null],
-      unidade: [null],
-      cargo: [null],
-      departamento: [null],
-      perfilDeAcesso: [null],
-      dataDeNascimento: [null]
-
+      nome: null,
+      dataDeAdmissao: null,
+      unidade: null,
+      cargo: null,
+      departamento: null,
+      perfilDeAcesso: null,
+      dataDeNascimento: null,
+      enderecoCep: null,
+      enderecoLogradouro: null,
+      enderecoBairro: null,
+      enderecoCidade: null,
+      enderecoEstado: null,
+      enderecoPais: null,
+      telefone: null,
+      email: null,
+      cpf: null,
+      salario: null,
+      descontos: this.formBuilder.array([]),
+      beneficios: this.formBuilder.array([]),
+      competencias: this.formBuilder.array([]),
+      registro: null,
+      senha: null
     })
   }
 
@@ -33,5 +46,56 @@ export class CadastrarColaboradorComponent {
 
   backToPainel() {
     this.router.navigate([`../painelAdm`])
+  }
+
+  addDesconto() {
+    const descontosArray = this.formCadastrarColaborador.get('descontos') as FormArray;
+    descontosArray.push(this.formBuilder.group({
+      tipoDesconto: '',
+      valorDesconto: null,
+    }));
+  }  
+
+  removeDesconto(index: number) {
+    const descontosArray = this.formCadastrarColaborador.get('descontos') as FormArray;
+    descontosArray.removeAt(index);
+  }
+
+  addBeneficio() {
+    const novoBeneficio = this.formBuilder.group({
+      tipoBeneficio: '',
+      valorBeneficio: null,
+    });
+    
+    (this.formCadastrarColaborador.get('beneficios') as FormArray).push(novoBeneficio);
+  }  
+
+  removeBeneficio(index: number) {
+    const beneficiosArray = this.formCadastrarColaborador.get('beneficios') as FormArray;
+    beneficiosArray.removeAt(index);
+  }
+
+  getBeneficios() {
+    const beneficiosArray = this.formCadastrarColaborador.get('beneficios') as FormArray;
+    return beneficiosArray.controls;
+  }
+
+  getDescontos() {
+    const descontosArray = this.formCadastrarColaborador.get('descontos') as FormArray;
+    return descontosArray.controls;
+  }
+
+  getCompetencias() {
+    const competenciasArray = this.formCadastrarColaborador.get('competencias') as FormArray;
+    return competenciasArray.controls;
+  }
+
+  addCompetencia() {
+    (this.formCadastrarColaborador.get('competencias') as FormArray).push('');
+  }  
+
+  removeCompetencia(index: number) {
+    const beneficiosArray = this.formCadastrarColaborador.get('competencias') as FormArray;
+    beneficiosArray.removeAt(index);
   }
 }
